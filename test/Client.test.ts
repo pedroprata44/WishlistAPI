abc8dda43ad8bc46b2372dc81da17332fae0dee0
pedroprata44 <contato.pedroprata@gmail.com>
2024-06-26 22:19:08 -0300

    test: create unitis test to client class

---
+++ test/Client.test.ts
@@ -0,0 +1,9 @@
+import Client from "../src/Client"
+
+test.each([undefined, null, "", "client"])("Should not do signup with a invalid name", function(name: any){
+    expect(() => new Client(name, "")).toThrow("Invalid name")
+})
+
+test.each([undefined, null, "", "@", "client.client"])("Should not do signup with a invalid email", function(email: any){
+    expect(() => new Client("client client", email)).toThrow("Invalid email")
+})
--- test/Signup.test.ts
+++ test/Signup.test.ts
@@ -14,19 +14,3 @@ test("Should return a email already exists when do signup", function(){
     const outputSignup = signup.execute(inputClient).accountEmail
     expect(outputSignup).toBe(inputClient.email)
 })
-
-test.each([undefined, null, "", "client"])("Should not do signup with a invalid name", function(name: any){
-    const inputClient = {
-        name: name,
-        email: "client@client"
-    }
-    expect(() => signup.execute(inputClient)).toThrow("Invalid name")
-})
-
-test.each([undefined, null, "", "@", "client.client"])("Should not do signup with a invalid email", function(email: any){
-    const inputClient = {
-        name: "client client",
-        email: email
-    }
-    expect(() => signup.execute(inputClient)).toThrow("Invalid email")
-})
