e454b6295b2db19c8029cc6f7db333800cdc1088
pedroprata44 <contato.pedroprata@gmail.com>
2024-06-29 19:05:45 -0300

    feat: not allows create a client with a email already register

--- src/usecases/CreateClient.ts
+++ src/usecases/CreateClient.ts
@@ -1,8 +1,11 @@
 import Client from "../domain/Client"
+import { clientsEmail } from "../domain/main"
 
 export default class CreateClient{
     execute(input: Input): Output{
+        if(clientsEmail.includes(input.email)) throw new Error("This email already register")
         const client = new Client(input.name, input.email)
+        clientsEmail.push(client.email.value)
         return {
             accountName: input.name,
             accountEmail: input.email
