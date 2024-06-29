93588ea374a5202e9a85118307ce9997ec268289
pedroprata44 <contato.pedroprata@gmail.com>
2024-06-29 19:08:08 -0300

    test: test new feat that not allows create a client with a email already exists

--- test/CreateClient.test.ts
+++ test/CreateClient.test.ts
@@ -1,3 +1,4 @@
+import { clientsEmail } from "../src/domain/main"
 import CreateClient from "../src/usecases/CreateClient"
 
 let createClient: CreateClient
@@ -14,4 +15,13 @@ test("Should create a client", function(){
     const outputCreateClient = createClient.execute(inputClient)
     expect(outputCreateClient.accountName).toBe(inputClient.name)
     expect(outputCreateClient.accountEmail).toBe(inputClient.email)
+})
+
+test("Should not create a client with a email already exists", function(){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    createClient.execute(inputClient)
+    expect(() => createClient.execute(inputClient)).toThrow(new Error("This email already register"))
 })
