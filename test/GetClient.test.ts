ecce642e34b0deb9c61dcf67c0cd2c2b6048305b
Pedro Prata <contato.pedroprata@gmail.com>
2024-07-01 11:42:14 -0300

    test: add test to getclient usecase

---
+++ test/GetClient.test.ts
@@ -0,0 +1,23 @@
+import CreateClient from "../src/usecases/CreateClient"
+import GetClient from "../src/usecases/GetClient"
+
+let createClient: CreateClient
+let getClient: GetClient
+
+beforeEach(() => {
+    createClient = new CreateClient()
+    getClient = new GetClient()
+})
+
+test("Should get a client", function(){
+    const inputClient = {
+        name: "client client",
+        email: "client@client"
+    }
+    
+    createClient.execute(inputClient)
+
+    const outputGetClient = getClient.execute(inputClient.email)
+    expect(outputGetClient.accountEmail).toBe(inputClient.email)
+    expect(outputGetClient.accountName).toBe(inputClient.name)
+})
