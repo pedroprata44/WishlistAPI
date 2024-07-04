723c76bb9f2146c199d11a3f6284c32a45e498cf
pedroprata44 <contato.pedroprata@gmail.com>
2024-07-04 13:29:17 -0300

    test: introducing tests to update client account in db

--- test/UpdateClient.test.ts
+++ test/UpdateClient.test.ts
@@ -1,51 +1,51 @@
-import Client from "../src/domain/Client"
+import DatabaseConnection from "../src/infra/database/DatabaseConnection"
+import PgPromiseAdapter from "../src/infra/database/PgPromiseAdapter"
+import ClientRepositoryDatabase from "../src/infra/repository/clientRepositoryDatabase"
+import ClientRepository from "../src/repository/ClientRepository"
 import CreateClient from "../src/usecases/CreateClient"
 import GetClient from "../src/usecases/GetClient"
 import UpdateClient from "../src/usecases/UpdateClient"
 
+let databaseConnection: DatabaseConnection
+let clientRepository: ClientRepository
 let createClient: CreateClient
 let getClient: GetClient
-let updateClient : UpdateClient
+let updateClient: UpdateClient
 
 beforeEach(() => {
-    createClient = new CreateClient()
-    getClient = new GetClient()
-    updateClient = new UpdateClient()
+    databaseConnection = new PgPromiseAdapter()
+    clientRepository = new ClientRepositoryDatabase(databaseConnection)
+    createClient = new CreateClient(clientRepository)
+    getClient = new GetClient(clientRepository)
+    updateClient = new UpdateClient(clientRepository)
 })
 
-test("Should update a client", function(){
+test("Should update a client", async function (){
     const inputClient = {
         name: "client client",
-        email: "client@client"
-    }
-    createClient.execute(inputClient)
-    const inputUpdateClient = {
-        name: "c c",
         email: `client${Math.random()}@client`
     }
-    updateClient.execute(inputClient.email, new Client(inputUpdateClient.name, inputUpdateClient.email))
-
-    const outputGetClient = getClient.execute(inputUpdateClient.email)
-    expect(outputGetClient.accountName).toBe(inputUpdateClient.name)
-    expect(outputGetClient.accountEmail).toBe(inputUpdateClient.email)
-})
-
-test("Should not update a client that not exists", function(){
-    expect(() => updateClient.execute("", new Client("client client", ".@."))).toThrow(new Error("Client not found"))
-})
-
-test("Should not update a client with a email already exists", function(){
-    const inputClient = {
-        name: "c c",
-        email: `client${Math.random()}@client`
-    }
-    createClient.execute(inputClient)
+    await createClient.execute(inputClient)
 
     const inputClient2 = {
-        name: "c c",
+        name: "client c",
         email: `client${Math.random()}@client`
     }
-    createClient.execute(inputClient2)
+    await updateClient.execute(inputClient.email, inputClient2)
+    
+    const outputGetClient = await getClient.execute(inputClient2.email)
+    expect(outputGetClient.accountEmail).toBeDefined()
+    expect(outputGetClient.accountName).toBe(inputClient2.name)
+})
 
-    expect(() => updateClient.execute(inputClient2.email, new Client(inputClient.name, inputClient.email))).toThrow(new Error("This email already register"))
+test("Should not update a client with a email that not exists", async function (){
+    const inputClient2 = {
+        name: "client c",
+        email: `client${Math.random()}@client`
+    }
+    await expect(updateClient.execute(inputClient2.email, inputClient2)).rejects.toThrow(new Error("Client not found"))
+})
+
+afterEach(() => {
+    databaseConnection.close()
 })
