fccce2c0aec507de75899dfc132909e20a2fc366
pedroprata44 <contato.pedroprata@gmail.com>
2024-07-06 10:24:42 -0300

    feat: remove main file from repository

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import Client from "./domain/Client";
-
-export let clients: Client[] = []
-
-export function removeClient(email: string){
-    clients = clients.filter(client => client.email.value != email)
-}
