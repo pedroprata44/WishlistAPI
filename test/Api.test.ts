77a736c1c09643c71c067fda8cafcc5aaea66cb6
Pedro Prata <contato.pedroprata@gmail.com>
2024-07-07 23:50:05 -0300

    test: introducing firsts tests to api

---
+++ test/Api.test.ts
@@ -0,0 +1,27 @@
+import axios from "axios"
+
+test("Should create a client by api", async function (){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseCreate = await axios.post("http://localhost:3000/createclient", inputClient)
+    const outputCreate = responseCreate.data
+
+    expect(outputCreate.accountEmail).toBe(inputClient.email)
+})
+
+test("Should get a client by api", async function (){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseCreate = await axios.post("http://localhost:3000/createclient", inputClient)
+    const outputCreate = responseCreate.data
+
+    const responseGet = await axios.get(`http://localhost:3000/getclient/${inputClient.email}`)
+    const outputGet = responseGet.data
+
+    expect(outputGet.accountEmail).toBe(inputClient.email)
+    expect(outputGet.accountName).toBe(inputClient.name)
+})
