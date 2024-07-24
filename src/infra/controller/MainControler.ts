512fbb916f263b687ceab36012f636024e48f237
pedroprata44 <contato.pedroprata@gmail.com>
2024-07-24 17:53:07 -0300

    feat: add getclient to api

--- src/infra/controller/MainControler.ts
+++ src/infra/controller/MainControler.ts
@@ -1,10 +1,15 @@
 import CreateClient from "../../application/usecases/CreateClient";
+import GetClient from "../../application/usecases/GetClient";
 import HttpServer from "../http/HttpServer";
 
 export default class MainControler{
-    constructor(readonly httpServer: HttpServer, createClient: CreateClient){
+    constructor(readonly httpServer: HttpServer, createClient: CreateClient, getClient: GetClient){
         this.httpServer.register("post", "/createclient", async function(params: any, body: any){
             const output = await createClient.execute(body)
+            return output
+        })
+        this.httpServer.register("get", "/getclient/:email", async function(params: any, body: any){
+            const output = await getClient.execute(params.email)
             return output
         })
     }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import PgPromiseAdapter from "./infra/database/PgPromiseAdapter";
 import ExpressAdapter from "./infra/http/ExpressAdapter";
 import ClientRepositoryDatabase from "./infra/repository/clientRepositoryDatabase";
 import CreateClient from "./application/usecases/CreateClient";
+import GetClient from "./application/usecases/GetClient";
 
 const httpServer = new ExpressAdapter()
 const dbConnection = new PgPromiseAdapter()
 const clientRepository = new ClientRepositoryDatabase(dbConnection)
 const createClient = new CreateClient(clientRepository)
-new MainControler(httpServer, createClient)
+const getClient = new GetClient(clientRepository)
+new MainControler(httpServer, createClient, getClient)
 httpServer.listen(3000)
