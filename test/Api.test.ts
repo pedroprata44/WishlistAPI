f5009dd7c1d3f8854d956dd7eedbb613e2c50d8c
pedroprata44 <contato.pedroprata@gmail.com>
2024-08-01 18:25:05 -0300

    feat: remove line and variable not used

--- test/Api.test.ts
+++ test/Api.test.ts
@@ -16,9 +16,7 @@ test("Should get a client by api", async function (){
         name: "client client",
         email: `client${Math.random()}@client`
     }
-    const responseCreate = await axios.post("http://localhost:3000/createclient", inputClient)
-    const outputCreate = responseCreate.data
-
+    await axios.post("http://localhost:3000/createclient", inputClient)
     const responseGet = await axios.get(`http://localhost:3000/getclient/${inputClient.email}`)
     const outputGet = responseGet.data
 
