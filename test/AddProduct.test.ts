2443b1401002c01f4a88a9a6812415bcadc01b40
pedroprata44 <contato.pedroprata@gmail.com>
2024-08-01 18:23:14 -0300

    test: complete test to addproduct to wishlist and add test to not allows add a product that does not exists

--- test/AddProduct.test.ts
+++ test/AddProduct.test.ts
@@ -1,5 +1,6 @@
 import AddProduct from "../src/application/usecases/AddProduct"
 import CreateClient from "../src/application/usecases/CreateClient"
+import GetWishlist from "../src/application/usecases/GetWishlist"
 import ListProducts from "../src/application/usecases/ListProducts"
 import DatabaseConnection from "../src/infra/database/DatabaseConnection"
 import PgPromiseAdapter from "../src/infra/database/PgPromiseAdapter"
@@ -12,6 +13,7 @@ let dbConnection: DatabaseConnection
 let createClient: CreateClient
 let clientRepository: ClientRepository
 let wishlistRepository: WishlistRepository
+let getWishlist: GetWishlist
 let addProduct: AddProduct
 let listProducts: ListProducts
 
@@ -20,6 +22,7 @@ beforeEach(() => {
     clientRepository = new ClientRepositoryDatabase(dbConnection)
     createClient = new CreateClient(clientRepository)
     wishlistRepository = new WishlistRepositoryDb(dbConnection)
+    getWishlist = new GetWishlist(clientRepository, wishlistRepository)
     addProduct = new AddProduct(clientRepository, wishlistRepository)
     listProducts = new ListProducts()
 })
@@ -31,14 +34,23 @@ test("Should add a product to wishlist", async function(){
     }
     const outputCreateClient = await createClient.execute(inputClient)
 
-
     const products = await listProducts.execute(5)
     const productId = products[5].id
-
     await addProduct.execute(outputCreateClient.accountEmail, productId)
 
     const wishlist = await getWishlist.execute(outputCreateClient.accountEmail)
-    expect(wishlist.includes(productId)).toBe(true)
+    expect(wishlist.productsId.find(product => product.product_id === productId)).toBeDefined()
+
+})
+
+test("Should not add a product does not exists", async function(){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const outputCreateClient = await createClient.execute(inputClient)
+    const productId = crypto.randomUUID()
+    await expect(addProduct.execute(outputCreateClient.accountEmail, productId)).rejects.toThrow(new Error("Product not found"))
 })
 
 afterEach(async () => {
