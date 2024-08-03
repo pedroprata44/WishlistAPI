b070ad4e6b828546a6f05cbc4c8ecff1d72db4d6
Pedro Prata <contato.pedroprata@gmail.com>
2024-08-03 07:16:01 -0300

    test: add test to getproduct usecase

---
+++ test/GetProduct.test.ts
@@ -0,0 +1,21 @@
+import GetProduct from "../src/application/usecases/GetProduct"
+import ListProducts from "../src/application/usecases/ListProducts"
+
+let getProduct: GetProduct
+let listProducts: ListProducts
+
+beforeEach(() => {
+    getProduct = new GetProduct()
+    listProducts = new ListProducts()
+})
+
+test("Should get products by products ids", async function(){
+    const productsPage = await listProducts.execute(1)
+    const productsId: string[] = []
+    for(const product of productsPage.slice(92)){
+        productsId.push(product.id)
+    }
+
+    const products = await getProduct.execute(productsId)
+    expect(products).toBeDefined()
+})
