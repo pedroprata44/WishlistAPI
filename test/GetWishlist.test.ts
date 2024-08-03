1dedd09b503352fbfc8434f5b9be50975ab650cb
pedroprata44 <contato.pedroprata@gmail.com>
2024-08-03 10:24:27 -0300

    test: add test to getwishlist usecase

---
+++ test/GetWishlist.test.ts
@@ -0,0 +1,52 @@
+import AddProduct from "../src/application/usecases/AddProduct"
+import CreateClient from "../src/application/usecases/CreateClient"
+import GetProduct from "../src/application/usecases/GetProduct"
+import GetWishlist from "../src/application/usecases/GetWishlist"
+import ListProducts from "../src/application/usecases/ListProducts"
+import DatabaseConnection from "../src/infra/database/DatabaseConnection"
+import PgPromiseAdapter from "../src/infra/database/PgPromiseAdapter"
+import ClientRepositoryDatabase from "../src/infra/repository/ClientRepositoryDatabase"
+import WishlistRepositoryDb from "../src/infra/repository/WishlistRepositoryDb"
+import ClientRepository from "../src/repository/ClientRepository"
+import WishlistRepository from "../src/repository/WishlistRepository"
+
+let dbConnection: DatabaseConnection
+let createClient: CreateClient
+let clientRepository: ClientRepository
+let listProducts: ListProducts
+let addProduct: AddProduct
+let wishlistRepository: WishlistRepository
+let getProduct: GetProduct
+let getWishlist: GetWishlist
+
+beforeEach(() => {
+    dbConnection = new PgPromiseAdapter()
+    clientRepository = new ClientRepositoryDatabase(dbConnection)
+    createClient = new CreateClient(clientRepository)
+    listProducts = new ListProducts()
+    getProduct = new GetProduct()
+    wishlistRepository = new WishlistRepositoryDb(dbConnection, getProduct)
+    addProduct = new AddProduct(clientRepository, wishlistRepository)
+    getWishlist = new GetWishlist(clientRepository, wishlistRepository)
+})
+
+test("Should get a existing wishlist", async function(){
+    const inputClient = {
+        name: "local host",
+        email: `client${Math.random()}@localhost.com`
+    }
+    const outputCreateClient = await createClient.execute(inputClient)
+
+    const page = await listProducts.execute(1)
+    const productId = page[0].id
+
+    await addProduct.execute(outputCreateClient.accountEmail, productId)
+    const wishlist = await getWishlist.execute(outputCreateClient.accountEmail)
+    const products = wishlist.products
+
+    expect(products.find(product => product.id === productId)).toBeDefined() 
+})
+
+afterEach(async ()=>{
+    await dbConnection.close()
+})
