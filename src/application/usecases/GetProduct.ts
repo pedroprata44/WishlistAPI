083a699ee4ec66b48f8a1fab395dfabab1834581
pedroprata44 <contato.pedroprata@gmail.com>
2024-08-03 10:20:11 -0300

    fix: fix bug that not gets the product id correctly in getproduct usecase

--- src/application/usecases/GetProduct.ts
+++ src/application/usecases/GetProduct.ts
@@ -2,11 +2,12 @@ import axios from "axios";
 import Product from "../domain/Product";
 
 export default class GetProduct{
+
     async execute(productsIds: any[]): Promise<Product[]>{
         try{
             const products: Product[] = []
             for(const productId of productsIds){
-                const responseGetProduct = await axios.get(`http://challenge-api.luizalabs.com/api/product/${productId}/`)
+                const responseGetProduct = await axios.get(`http://challenge-api.luizalabs.com/api/product/${productId.product_id}/`)
                 const outputGetProduct = responseGetProduct.data
                 const product = new Product(outputGetProduct.price, outputGetProduct.image, outputGetProduct.brand, outputGetProduct.id, outputGetProduct.title)
                 products.push(product)
