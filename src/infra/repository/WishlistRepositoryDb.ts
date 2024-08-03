079431226845387c01cfebc8e8633df253ede9ec
pedroprata44 <contato.pedroprata@gmail.com>
2024-08-03 10:20:52 -0300

    feat: change name and type of productsids iterator

--- src/infra/repository/WishlistRepositoryDb.ts
+++ src/infra/repository/WishlistRepositoryDb.ts
@@ -10,9 +10,9 @@ export default class WishlistRepositoryDb implements WishlistRepository{
         await this.dbConnection.query("insert into data.wishlist (client_email, product_id) values ($1, $2)", [clientEmail, productId])
     }
     async getByEmail(clientEmail: string): Promise<Wishlist>{  
-        const products_ids: any[] = await this.dbConnection.query("select product_id from data.wishlist where client_email = $1", [clientEmail])
-        if(!products_ids) throw new Error("Empty wishlist")
-        const products = await this.getProduct.execute(products_ids)
+        const productsIds = await this.dbConnection.query("select product_id from data.wishlist where client_email = $1", [clientEmail])
+        if(!productsIds) throw new Error("Empty wishlist")
+        const products = await this.getProduct.execute(productsIds)
         if(!products) throw new Error("Product id not found")
         const output = Wishlist.restore(clientEmail, products)
         return output
