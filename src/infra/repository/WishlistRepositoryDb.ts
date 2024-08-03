d6c46e58224c64e51a6a887529c3f6f30497e051
Pedro Prata <contato.pedroprata@gmail.com>
2024-08-03 07:15:15 -0300

    feat: add getproduct usecase to wishlistrepositorydb

--- src/infra/repository/WishlistRepositoryDb.ts
+++ src/infra/repository/WishlistRepositoryDb.ts
@@ -1,17 +1,20 @@
 import Wishlist from "../../application/domain/Wishlist";
+import GetProduct from "../../application/usecases/GetProduct";
 import WishlistRepository from "../../repository/WishlistRepository";
 import DatabaseConnection from "../database/DatabaseConnection";
 
 export default class WishlistRepositoryDb implements WishlistRepository{
-    constructor(readonly dbConnection: DatabaseConnection){
+    constructor(private dbConnection: DatabaseConnection, private getProduct: GetProduct){
     }
     async save(clientEmail: string, productId: string){
         await this.dbConnection.query("insert into data.wishlist (client_email, product_id) values ($1, $2)", [clientEmail, productId])
     }
     async getByEmail(clientEmail: string): Promise<Wishlist>{  
-        const products_ids = await this.dbConnection.query("select product_id from data.wishlist where client_email = $1", [clientEmail])
+        const products_ids: any[] = await this.dbConnection.query("select product_id from data.wishlist where client_email = $1", [clientEmail])
         if(!products_ids) throw new Error("Empty wishlist")
-        const output = Wishlist.restore(clientEmail, products_ids)
+        const products = await this.getProduct.execute(products_ids)
+        if(!products) throw new Error("Product id not found")
+        const output = Wishlist.restore(clientEmail, products)
         return output
     }
 }
