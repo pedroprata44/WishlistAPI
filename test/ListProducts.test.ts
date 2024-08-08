fb7f5f650f1e893a3a8a14bde43ad7545a3add07
pedroprata44 <contato.pedroprata@gmail.com>
2024-08-08 14:49:03 -0300

    test: fix listproducts test to new feat product repository

--- test/ListProducts.test.ts
+++ test/ListProducts.test.ts
@@ -1,12 +1,17 @@
 import ListProducts from "../src/application/usecases/ListProducts"
+import ProductRepositoryApi from "../src/infra/repository/ProductRepositoryApi"
+import ProductRepository from "../src/repository/ProductRepository"
 
 let listProducts: ListProducts
+let productRepository: ProductRepository
 
 beforeEach(() => {
-    listProducts = new ListProducts()
+    productRepository = new ProductRepositoryApi()
+    listProducts = new ListProducts(productRepository)
 })
 
 test("Should return a list of products", async function(){
-    const products = await listProducts.execute(1)
+    const products = await listProducts.execute("1")
     expect(products).toBeDefined()
+    
 })
