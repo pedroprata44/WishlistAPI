9c95f1e85b022b3aefb75124f911f8f0b5dc00e7
Pedro Prata <contato.pedroprata@gmail.com>
2024-08-26 23:29:43 -0300

    feat: add part of use cases to api rest

--- src/infra/controller/MainControler.ts
+++ src/infra/controller/MainControler.ts
@@ -1,15 +1,32 @@
+import AddProduct from "../../application/usecases/AddProduct";
 import CreateClient from "../../application/usecases/CreateClient";
 import GetClient from "../../application/usecases/GetClient";
+import GetProduct from "../../application/usecases/GetProduct";
+import GetWishlist from "../../application/usecases/GetWishlist";
+import RemoveClient from "../../application/usecases/RemoveClient";
+import UpdateClient from "../../application/usecases/UpdateClient";
 import HttpServer from "../http/HttpServer";
 
 export default class MainControler{
-    constructor(readonly httpServer: HttpServer, createClient: CreateClient, getClient: GetClient){
+    constructor(readonly httpServer: HttpServer, createClient: CreateClient, getClient: GetClient,   getWishlist: GetWishlist, getProduct: GetProduct, addProduct: AddProduct){
         this.httpServer.register("post", "/createclient", async function(params: any, body: any){
             const output = await createClient.execute(body)
             return output
         })
         this.httpServer.register("get", "/getclient/:email", async function(params: any, body: any){
             const output = await getClient.execute(params.email)
+            return output
+        })
+        this.httpServer.register("post", "/addProduct/:email", async function (params: any, body: any) {
+            const output = await addProduct.execute(params.email, body)
+            return output
+        })
+        this.httpServer.register("get", "/getWishlist/:email", async function (params: any, body: any) {
+            const output = await getWishlist.execute(params.email)
+            return output
+        })
+        this.httpServer.register("get", "/getProduct/:id", async function (params: any, body: any){
+            const output = await getProduct.execute(params.id)
             return output
         })
     }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import MainControler from "./infra/controller/MainControler";
 import PgPromiseAdapter from "./infra/database/PgPromiseAdapter";
 import ExpressAdapter from "./infra/http/ExpressAdapter";
-import ClientRepositoryDatabase from "./infra/repository/ClientRepositoryDatabase";
+import ClientRepositoryDatabase from "./infra/repository/clientRepositoryDatabase";
 import CreateClient from "./application/usecases/CreateClient";
 import GetClient from "./application/usecases/GetClient";
+import GetWishlist from "./application/usecases/GetWishlist";
+import WishlistRepositoryDb from "./infra/repository/WishlistRepositoryDb";
+import GetProduct from "./application/usecases/GetProduct";
+import ProductRepositoryApi from "./infra/repository/ProductRepositoryApi";
+import AddProduct from "./application/usecases/AddProduct";
 
 const httpServer = new ExpressAdapter()
 const dbConnection = new PgPromiseAdapter()
 const clientRepository = new ClientRepositoryDatabase(dbConnection)
 const createClient = new CreateClient(clientRepository)
 const getClient = new GetClient(clientRepository)
-new MainControler(httpServer, createClient, getClient)
+const productRepository = new ProductRepositoryApi()
+const getProduct = new GetProduct(productRepository)
+const wishlistRepository = new WishlistRepositoryDb(dbConnection, getProduct)
+const getWishlist = new GetWishlist(clientRepository, wishlistRepository)
+const addProduct = new AddProduct(clientRepository, wishlistRepository, productRepository)
+new MainControler(httpServer, createClient, getClient, getWishlist, getProduct, addProduct)
 httpServer.listen(3000)
