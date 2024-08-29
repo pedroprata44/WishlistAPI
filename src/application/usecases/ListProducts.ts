678734b1cb6f038ecd0dbfcfa029ad8899c358e4
Pedro Prata <contato.pedroprata@gmail.com>
2024-08-29 00:01:50 -0300

    feat: remove case listproducts

--- src/application/usecases/ListProducts.ts
+++
@@ -1,9 +0,0 @@
-import ProductRepository from "../../repository/ProductRepository";
-
-export default class ListProducts{
-    constructor(private productRepository: ProductRepository){
-    }
-    async execute(pageNumber: string){
-        return await this.productRepository.getProductsByPageNumber(pageNumber)
-    }
-}
