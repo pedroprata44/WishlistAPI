d4b258a46f224063199252f43cadd4019a77d966
pedroprata44 <contato.pedroprata@gmail.com>
2024-09-04 18:39:47 -0300

    update: add enviroment vars to string db connection

--- src/infra/database/PgPromiseAdapter.ts
+++ src/infra/database/PgPromiseAdapter.ts
@@ -1,10 +1,22 @@
 import DatabaseConnection from "./DatabaseConnection";
 import pgp from "pg-promise"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default class PgPromiseAdapter implements DatabaseConnection{
     connection: any
+    postgresUser: string
+    postgresPassword: string
+    postgresServer: string
+    postgresPort: string
     constructor(){
-        this.connection = pgp()("postgres://postgres:password@localhost:5432/wishlist")
+        this.postgresUser = process.env.POSTGRES_USER || "postgres"
+        this.postgresPassword = process.env.POSTGRES_PASSWORD || "password"
+        this.postgresServer = process.env.POSTGRES_SERVER || "wishlist"
+        this.postgresPort = process.env.POSTGRES_PORT || "5432"
+
+        this.connection = pgp()(`postgres://${this.postgresUser}:${this.postgresPassword}@localhost:${this.postgresPort}/${this.postgresServer}`)
     }
     query(statement: string, params: any): Promise<any> {
         return this.connection.query(statement, params)
