7f7e003a7a30ded81586a38cc9dcd84e2fd9ee25
pedroprata44 <contato.pedroprata@gmail.com>
2024-09-05 14:56:12 -0300

    test: add test to addproduct by api

--- test/Api.test.ts
+++ test/Api.test.ts
@@ -1,7 +1,10 @@
 import axios from "axios"
+import { log } from "console"
 import dotenv from 'dotenv'
 
-dotenv.config()
+beforeAll(() => {
+    dotenv.config()
+})
 
 test("Should create a client and resolves a token by api", async function (){
     const inputClient = {
@@ -19,10 +22,37 @@ test("Should get a client by api", async function (){
         name: "client client",
         email: `client${Math.random()}@client`
     }
-    await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/createclient`, inputClient)
-    const responseGet = await axios.get(`http://localhost:${process.env.WISHLIST_PORT}/getclient/${inputClient.email}`)
-    const outputGet = responseGet.data
+    const responseCreateClient = await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/createclient`, inputClient)
+    const token = responseCreateClient.data
 
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const responseGet = await axios.get(`http://localhost:${process.env.WISHLIST_PORT}/getclient/${inputClient.email}`, config)
+    const outputGet = responseGet.data
     expect(outputGet.accountEmail).toBe(inputClient.email)
     expect(outputGet.accountName).toBe(inputClient.name)
+})
+
+test("Should add a product to wishlist by api", async function (){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseCreateClient = await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/createclient`, inputClient)
+    const token = responseCreateClient.data
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const productId = "1"
+
+    await axios.post(`http://localhost:3000/addproduct/${inputClient.email}`, {productId: productId}, config)
+    const responseGetWishlist = await axios.get(`http://localhost:${process.env.WISHLIST_PORT}/getwishlist/${inputClient.email}`, config)
+    const outputGetWilist = responseGetWishlist.data
+
+    expect(outputGetWilist.products[0].id).toBe(productId)
 })
