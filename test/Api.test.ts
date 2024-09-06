8a08a5e1d435b43d27cbb07a81fe1d1d7b8d1b8e
pedroprata44 <contato.pedroprata@gmail.com>
2024-09-06 16:26:09 -0300

    test: add tests to all use cases by api validate status of axios always returns true

--- test/Api.test.ts
+++ test/Api.test.ts
@@ -1,11 +1,15 @@
 import axios from "axios"
 import dotenv from 'dotenv'
 
+axios.defaults.validateStatus = function (){
+    return true
+}
+
 beforeAll(() => {
     dotenv.config()
 })
 
-test("Should create a client and resolves a token by api", async function (){
+test("Should create a client by api", async function (){
     const inputClient = {
         name: "client client",
         email: `client${Math.random()}@client`
@@ -35,6 +39,48 @@ test("Should get a client by api", async function (){
     expect(outputGet.accountName).toBe(inputClient.name)
 })
 
+test("Should update a client by api", async function (){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseCreateClient = await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/createclient`, inputClient)
+    const token = responseCreateClient.data
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const updateClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseUpdateClient = await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/updateclient/${inputClient.email}`, updateClient, config)
+    const outputGet = responseUpdateClient.data
+
+    expect(outputGet.nameUpdated).toBe(updateClient.name)
+    expect(outputGet.emailUpdated).toBe(updateClient.email)
+})
+
+test("Should remove a client by api", async function(){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseCreateClient = await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/createclient`, inputClient)
+    const token = responseCreateClient.data
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/removeclient/${inputClient.email}`, "", config)
+
+    const responseGetClient = await axios.get(`http://localhost:${process.env.WISHLIST_PORT}/getclient/${inputClient.email}`, config)
+    expect(responseGetClient.data.message).toBe("Client not found")
+})
+
 test("Should add a product to wishlist by api", async function (){
     const inputClient = {
         name: "client client",
@@ -54,4 +100,24 @@ test("Should add a product to wishlist by api", async function (){
     const outputGetWilist = responseGetWishlist.data
 
     expect(outputGetWilist.products[0].id).toBe(productId)
+})
+
+test("Should get a product by api", async function(){
+    const inputClient = {
+        name: "client client",
+        email: `client${Math.random()}@client`
+    }
+    const responseCreateClient = await axios.post(`http://localhost:${process.env.WISHLIST_PORT}/createclient`, inputClient)
+    const token = responseCreateClient.data
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const responseGetProduct = await axios.get(`http://localhost:${process.env.WISHLIST_PORT}/getproduct/1`, config)
+    const product = responseGetProduct.data
+
+    console.log(product);
+    expect(product.id).toBeDefined()
 })
